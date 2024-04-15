f4fca5cea8318bf850ae4cd929a7a26573ab34f1
ahmedx33 <faresahmed51857@gmail.com>
2024-04-15 21:09:24 +0200

    feat: Added userDataNav

--- src/components/learn/user-data-nav.tsx
+++ src/components/learn/user-data-nav.tsx
@@ -4,18 +4,20 @@ import { FaHeart } from "react-icons/fa";
 
 export default function UserDataNav() {
     return (
-        <nav className="w-[400px] h-screen">
+        <nav className="w-[400px] h-screen px-4 py-7 flex justify-center">
             <section>
-                <span>
-                    <RiLightbulbFlashFill />
-                </span>
+                <section className="flex items-center">
+                    <span>
+                        <RiLightbulbFlashFill color="#1CB0F6" size={30} />
+                    </span>
 
-                <span>
-                    <FaHeart />
-                </span>
+                    <span>
+                        <FaHeart color="#FF4B4B" size={30} />
+                    </span>
+                </section>
+
+                <section>Hello world</section>
             </section>
-
-            <section>Hello world</section>
         </nav>
     );
 }
