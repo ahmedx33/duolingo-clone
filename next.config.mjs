3f3df3aabc9fca4df1442079be88ba2f145241d6
ahmedx33 <faresahmed51857@gmail.com>
2024-04-17 17:52:41 +0200

    feat: Added svgs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "d2pur3iezf4d1j.cloudfront.net",
+                hostname: "ljfujfxjgcubnqfhhlxe.supabase.co",
             },
         ],
     },
