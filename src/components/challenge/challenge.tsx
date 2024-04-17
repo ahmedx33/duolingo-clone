0d58e3a330efc6785138371053da29d6ae06231a
ahmedx33 <faresahmed51857@gmail.com>
2024-04-17 21:42:14 +0200

    feat: Added useMemo and filterd the challenges option

--- src/components/challenge/challenge.tsx
+++ src/components/challenge/challenge.tsx
@@ -19,14 +19,12 @@ export default function Challenge({
         return challengeOptions.filter((challengeOption) => challengeOption.challengeId === id);
     }, [challengeOptions, id]);
 
-    console.log(getCurrentChallengeOptions);
-
     return (
         <div>
             <h1 className="mb-[27px] text-[2rem] font-semibold text-[#404040]">{question}</h1>
 
             <div className="flex items-center gap-2">
-                {challengeOptions?.map((challengeOption, idx) => (
+                {getCurrentChallengeOptions?.map((challengeOption, idx) => (
                     <Card key={challengeOption.id} {...challengeOption} type={type} setSelected={setSelected} selected={selected === challengeOption.id} keys={idx + 1} />
                 ))}
             </div>
