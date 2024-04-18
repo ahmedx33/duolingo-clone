3676568bab7390224af042c16c1a0f914df70849
ahmedx33 <faresahmed51857@gmail.com>
2024-04-18 20:36:32 +0200

    feat: added " SELECT " type for challenge and card components

--- src/components/challenge/card.tsx
+++ src/components/challenge/card.tsx
@@ -12,7 +12,7 @@ interface MoreOptions {
     type: "ASSIST" | "SELECT";
     keys: number;
     isSelected: boolean;
-    selected: string | undefined
+    selected: string | undefined;
     setSelected: Dispatch<SetStateAction<string | undefined>>;
     setSelectedCardStatus: Dispatch<SetStateAction<boolean | undefined>>;
     isCorrect: boolean | undefined;
@@ -29,6 +29,7 @@ export function Card({ id, type, text, imageSrc, correct, setSelected, isSelecte
         setSelectedCardStatus(correct);
     };
 
+
     useKey(String(keys), selectHandler, { event: "keydown" });
 
     return (
@@ -39,21 +40,13 @@ export function Card({ id, type, text, imageSrc, correct, setSelected, isSelecte
                 className={cn(
                     "flex flex-col items-center gap-y-7 h-[235px] w-[200px] border-2 rounded-xl border-b-4 p-4 lg:p-6 cursor-pointer ",
                     isSelected ? "border-sky-300 bg-sky-100 hover:bg-sky-100" : "",
-                   
+                    type === "SELECT" && "h-fit w-[500px]"
                 )}
             >
-                <Image src={imageSrc as string} alt="pic" width={100} height={100} />
-                <div className="flex items-center justify-between w-full">
-                    <p className="text-[#404040] font-bold text-[1.1rem]">{text}</p>
-                    <div
-                        className={cn(
-                            "text-[#A3A3A3] border-2 border-[#b1b8c2]  px-2 rounded-lg",
-                            isSelected && "border-sky-300 text-sky-500",
-                
-                        )}
-                    >
-                        {keys}
-                    </div>
+                {type === "ASSIST" ? <Image src={imageSrc as string} alt="pic" width={100} height={100} /> : ""}
+                <div className={cn("flex items-center justify-between w-full", type === "SELECT" && "flex-row-reverse")}>
+                    <p className={cn("text-[#404040] font-bold text-[1.1rem]", type === "SELECT" && "w-full")}>{text}</p>
+                    <div className={cn("text-[#A3A3A3] border-2 border-[#b1b8c2]  px-2 rounded-lg", isSelected && "border-sky-300 text-sky-500")}>{keys}</div>
                 </div>
             </Button>
         </main>
--- src/components/challenge/challenge.tsx
+++ src/components/challenge/challenge.tsx
@@ -11,6 +11,7 @@ import { IoMdClose, IoMdCheckmark } from "react-icons/io";
 import axios from "axios";
 import { cn } from "@/lib/utils";
 import { useAudio } from "react-use";
+import Image from "next/image";
 
 export default function Challenge({
     id,
@@ -31,15 +32,18 @@ export default function Challenge({
     const [selectedCardStatus, setSelectedCardStatus] = useState<boolean>();
     const [isCorrect, setIsCorrect] = useState<boolean>();
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const splittedQuestion = question?.split('"');
+
     const [correct, _, correctControls] = useAudio({
         src: "/sounds/duolingo-correct.mp3",
     });
     const [wrong, s, wrongControls] = useAudio({
         src: "/sounds/duolingo-wrong.mp3",
     });
+
     const userProgress = useSelector((state: RootState) => state.userProgress.value);
     const isDisabled = selected === undefined;
-    console.log(isCorrect);
+
     const getCurrentChallengeOptions = useMemo(() => {
         return challengeOptions.filter((challengeOption) => challengeOption.challengeId === id);
     }, [challengeOptions, id]);
@@ -70,9 +74,20 @@ export default function Challenge({
         <div>
             {correct}
             {wrong}
-            <h1 className="mb-[27px] text-[2rem] font-semibold text-[#404040]">{question}</h1>
 
-            <div className="flex items-center gap-2">
+            {type === "SELECT" && (
+                <div className="absolute top-[175px] ">
+                    <Image src="https://ljfujfxjgcubnqfhhlxe.supabase.co/storage/v1/object/public/icons/boy.svg" alt="welcome" width={70} height={70} draggable={false} />
+                    <div className="absolute top-1/2 -translate-y-1/2 w-[70px] py-2 -right-[76px] border-2 border-[#E2E8F0] text-center rounded-lg">
+                        <div className="absolute  border-[10px] border-transparent border-r-[#E2E8F0] left-[-20px] top-1/2 -translate-y-1/2"></div>
+                        {splittedQuestion[1]}
+                    </div>
+                </div>
+            )}
+
+            <h1 className="mb-[27px] text-[2rem] font-semibold text-[#404040]">{type === "SELECT" ? splittedQuestion[0] : question}</h1>
+
+            <div className={cn("flex items-center gap-2", type === "SELECT" && "flex-col")}>
                 {getCurrentChallengeOptions?.map((challengeOption, idx) => (
                     <Card
                         key={challengeOption.id}
