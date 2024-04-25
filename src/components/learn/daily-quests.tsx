05b4b7f63f9cd393107284267e1b547a2d30cfbc
ahmedx33 <faresahmed51857@gmail.com>
2024-04-25 19:38:54 +0200

    edit: removed questProgress function and added it to main comp

--- src/components/learn/daily-quests.tsx
+++ src/components/learn/daily-quests.tsx
@@ -17,6 +17,7 @@ export default function DailyQuests() {
     const currentUser = useSelector((state: RootState) => state.userProgress.value)
     const totalPoints = currentUser?.points
 
+    console.log(totalPoints)
     return (
         <main className="border-[#DDDDDD] border-2 rounded-xl py-3 px-5 flex flex-col  min-fit w-[350px] mb-6">
             <h1 className="mb-4 flex items-center justify-between">
@@ -25,19 +26,13 @@ export default function DailyQuests() {
                     View All
                 </Link>
             </h1>
-            <QuestProgress value={20} totalPoints={totalPoints}/>
-        </main>
-    );
-}
-
-function QuestProgress({ value, totalPoints }: QuestProgressProps) {
-    return (
-        <section className="mb-3">
-            <h1 className="ml-[55px] text-[#4B4B4B] font-bold">Earn {value} XP</h1>
+            <section className="mb-3">
+            <h1 className="ml-[55px] text-[#4B4B4B] font-bold">Earn {20} XP</h1>
             <span className="w-full flex items-center gap-x-3">
                 <Image src="/svg/quest.svg" alt="quest" width={40} height={40} />
-                <Progress value={totalPoints} className="h-3 bg-[#FACC15]" />
+                <Progress value={totalPoints} className="bg-[#FACC15]" />
             </span>
         </section>
+        </main>
     );
 }
