2ab905be46f20323028ed487871379b8882d16ef
ahmedx33 <faresahmed51857@gmail.com>
2024-04-25 19:39:43 +0200

    edit: changed the quest value to 100

--- src/components/learn/daily-quests.tsx
+++ src/components/learn/daily-quests.tsx
@@ -17,7 +17,6 @@ export default function DailyQuests() {
     const currentUser = useSelector((state: RootState) => state.userProgress.value)
     const totalPoints = currentUser?.points
 
-    console.log(totalPoints)
     return (
         <main className="border-[#DDDDDD] border-2 rounded-xl py-3 px-5 flex flex-col  min-fit w-[350px] mb-6">
             <h1 className="mb-4 flex items-center justify-between">
@@ -27,7 +26,7 @@ export default function DailyQuests() {
                 </Link>
             </h1>
             <section className="mb-3">
-            <h1 className="ml-[55px] text-[#4B4B4B] font-bold">Earn {20} XP</h1>
+            <h1 className="ml-[55px] text-[#4B4B4B] font-bold">Earn 100 XP</h1>
             <span className="w-full flex items-center gap-x-3">
                 <Image src="/svg/quest.svg" alt="quest" width={40} height={40} />
                 <Progress value={totalPoints} className="bg-[#FACC15]" />
