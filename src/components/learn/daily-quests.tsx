3b4f05efbaebdafc7e6f2859567537fd86fb562d
ahmedx33 <faresahmed51857@gmail.com>
2024-04-25 18:48:19 +0200

    feat: added class name to progress bar to change the color with itand changed the color  to every comp has this progress bar

--- src/components/learn/daily-quests.tsx
+++ src/components/learn/daily-quests.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 import { Progress } from "../ui/progress";
 
-import { BsFillLightningChargeFill } from "react-icons/bs";
 import { useSelector } from "react-redux";
 import { RootState } from "@/lib/store";
+import Image from "next/image";
 
 interface QuestProgressProps {
     value: number;
@@ -18,7 +18,7 @@ export default function DailyQuests() {
     const totalPoints = currentUser?.points
 
     return (
-        <main className="border-[#DDDDDD] border-2 rounded-xl py-3 px-5 flex flex-col  min-h-48 w-[350px] mb-6">
+        <main className="border-[#DDDDDD] border-2 rounded-xl py-3 px-5 flex flex-col  min-fit w-[350px] mb-6">
             <h1 className="mb-4 flex items-center justify-between">
                 <span className="text-[#4B4B4B] font-bold text-[1.1rem]">Daily Quests</span>
                 <Link href="/quests" className="uppercase text-[#1CB0F6] font-bold">
@@ -26,10 +26,6 @@ export default function DailyQuests() {
                 </Link>
             </h1>
             <QuestProgress value={20} totalPoints={totalPoints}/>
-            <QuestProgress value={50} totalPoints={totalPoints}/>
-            <QuestProgress value={100} totalPoints={totalPoints}/>
-            <QuestProgress value={500} totalPoints={totalPoints}/>
-            <QuestProgress value={1000} totalPoints={totalPoints}/>
         </main>
     );
 }
@@ -37,10 +33,10 @@ export default function DailyQuests() {
 function QuestProgress({ value, totalPoints }: QuestProgressProps) {
     return (
         <section className="mb-3">
-            <h1 className="ml-[40px] text-[#4B4B4B] font-bold">Earn {value} XP</h1>
+            <h1 className="ml-[55px] text-[#4B4B4B] font-bold">Earn {value} XP</h1>
             <span className="w-full flex items-center gap-x-3">
-                <BsFillLightningChargeFill size={30} color="#F7C100" />
-                <Progress value={totalPoints} className="h-3" />
+                <Image src="/svg/quest.svg" alt="quest" width={40} height={40} />
+                <Progress value={totalPoints} className="h-3 bg-[#FACC15]" />
             </span>
         </section>
     );
