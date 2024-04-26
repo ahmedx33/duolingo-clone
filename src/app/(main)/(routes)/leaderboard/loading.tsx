6b078c4cb2f25e302e3628829fa2ed437188a95a
ahmedx33 <faresahmed51857@gmail.com>
2024-04-26 21:06:00 +0300

    fix: spinner page width and height

--- src/app/(main)/(routes)/leaderboard/loading.tsx
+++ src/app/(main)/(routes)/leaderboard/loading.tsx
@@ -3,7 +3,7 @@ import Spinner from "@/components/spinner";
 export default function Loading() {
 
 
-    return <div className="flex items-center justify-center">
+    return <div className="flex items-center justify-center w-full h-full">
         <Spinner />
     </div>
 }
