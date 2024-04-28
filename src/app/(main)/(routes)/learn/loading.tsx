f2ec0bb0a4d3bc22415dc6dd883f2cda43d4a3cb
ahmedx33 <faresahmed51857@gmail.com>
2024-04-29 00:12:54 +0300

    feat: added loading page to lessons page

---
+++ src/app/(main)/(routes)/learn/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from "@/components/spinner";
+
+export default function Loading() {
+    return (
+        <main className="flex items-center justify-center w-full h-screen">
+            <Spinner />
+        </main>
+    );
+}
