12bd7e385ec3d39a6758009fafa40d8791f739a4
ahmedx33 <faresahmed51857@gmail.com>
2024-04-29 16:47:31 +0300

    fix: hide bottom links from large screen

--- src/components/learn/bottom-links.tsx
+++ src/components/learn/bottom-links.tsx
@@ -5,7 +5,7 @@ import { SiDuolingo } from "react-icons/si";
 
 export function BottomLinks() {
     return (
-        <nav className="w-full h-20 border-[#E5E5E5] border-t-2 px-4 py-7 flex left-0 bottom-0 bg-white  max-md:fixed">
+        <nav className="w-full h-20 border-[#E5E5E5] border-t-2 px-4 py-7 hidden max-md:flex left-0 bottom-0 bg-white  max-md:fixed ">
             <ul className="flex items-center gap-x-2 w-full max-lg:justify-center">
                 {LINKS.map((link) => (
                     <li key={link.title} className="w-full">
