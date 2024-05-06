cc6bdbe8ce40cbd852db2fc86de517cbb5f96a74
ahmedx33 <faresahmed51857@gmail.com>
2024-05-06 14:13:31 +0300

    feat: added loading to main routes

--- src/app/(main)/(routes)/leaderboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Spinner from "@/components/spinner";
-
-export default function Loading() {
-
-
-    return <div className="flex items-center justify-center w-full h-full">
-        <Spinner />
-    </div>
-}
