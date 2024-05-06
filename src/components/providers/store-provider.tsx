ca08de5f8d02a0b828d77bad7b7a082d95afa97b
ahmedx33 <faresahmed51857@gmail.com>
2024-05-06 18:28:59 +0300

    edit: format

--- src/components/providers/store-provider.tsx
+++ src/components/providers/store-provider.tsx
@@ -2,12 +2,13 @@
 import { store } from "@/lib/store";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
-import {createPortal} from "react-dom"
+import { createPortal } from "react-dom";
 
 export default function StoreProvider({ children }: { children: ReactNode }) {
     return createPortal(
         <div>
             <Provider store={store}>{children}</Provider>
-        </div>, document.querySelector("body") as Element
-    )
+        </div>,
+        document.querySelector("body") as Element
+    );
 }
