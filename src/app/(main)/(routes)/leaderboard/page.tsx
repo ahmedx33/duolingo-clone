d9cc1bf29f8b5514df1e19bf3cc81bfe155c1bec
ahmedx33 <faresahmed51857@gmail.com>
2024-05-07 20:07:58 +0300

    edit: removes emptys

--- src/app/(main)/(routes)/leaderboard/page.tsx
+++ src/app/(main)/(routes)/leaderboard/page.tsx
@@ -1,12 +1,12 @@
-
-
 import Loading from "@/app/loading";
 import dynamic from "next/dynamic";
 
 const LeaderboardPageContent = dynamic(() => import("@/components/learn/learn-page-content"), { loading: () => <Loading /> });
 
 export default function Page() {
-    return <>
-    <LeaderboardPageContent />
-    </>
+    return (
+        <>
+            <LeaderboardPageContent />
+        </>
+    );
 }
