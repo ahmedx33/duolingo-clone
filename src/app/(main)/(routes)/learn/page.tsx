eeeb7a6a652dfd1a16707751bfb8450a069a21fe
ahmedx33 <faresahmed51857@gmail.com>
2024-05-07 15:04:35 +0300

    feat: added loading to learn page content

--- src/app/(main)/(routes)/learn/page.tsx
+++ src/app/(main)/(routes)/learn/page.tsx
@@ -1,7 +1,8 @@
+import Loading from "@/app/loading";
 import { BottomLinks } from "@/components/learn/bottom-links";
 import dynamic from "next/dynamic";
 
-const LearnPageContent = dynamic(() => import("@/components/learn/learn-page-content"), { loading: () => <h1>loading...</h1> });
+const LearnPageContent = dynamic(() => import("@/components/learn/learn-page-content"), { loading: () => <Loading /> });
 
 export default function Page() {
     return (
