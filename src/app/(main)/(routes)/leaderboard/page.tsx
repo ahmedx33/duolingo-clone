79fdb8a31bc206355aea84df502b77118f4539c0
ahmedx33 <faresahmed51857@gmail.com>
2024-05-08 18:47:36 +0300

    fix: dynamic import path

--- src/app/(main)/(routes)/leaderboard/page.tsx
+++ src/app/(main)/(routes)/leaderboard/page.tsx
@@ -1,7 +1,7 @@
 import Loading from "@/app/loading";
 import dynamic from "next/dynamic";
 
-const LeaderboardPageContent = dynamic(() => import("@/components/learn/learn-page-content"), { loading: () => <Loading /> });
+const LeaderboardPageContent = dynamic(() => import("@/components/leaderboard/leaderboard-page-content"), { loading: () => <Loading /> });
 
 export default function Page() {
     return (
--- src/components/leaderboard/leaderboard-page-content.tsx
+++ src/components/leaderboard/leaderboard-page-content.tsx
@@ -2,7 +2,7 @@ import { ActiveUsersList } from "@/components/leaderboard/active-users-list";
 import { LeaderboardHeader } from "@/components/leaderboard/leaderboard-header";
 import { BottomLinks } from "@/components/learn/bottom-links";
 
-export function LeaderboardPageContent() {
+export default function LeaderboardPageContent() {
     return (
         <main className="w-full h-screen flex flex-col items-center py-5">
             <LeaderboardHeader />
