f5fd724b61bbfea749956fcd6a8999b3e45944dd
ahmedx33 <faresahmed51857@gmail.com>
2024-05-10 15:52:02 +0300

    feat: added redirect in learn page when unauthorized user

--- src/app/(main)/(routes)/learn/page.tsx
+++ src/app/(main)/(routes)/learn/page.tsx
@@ -1,12 +1,16 @@
 
 import Loading from "@/app/loading";
 import { BottomLinks } from "@/components/learn/bottom-links";
+import { auth } from "@clerk/nextjs";
 import dynamic from "next/dynamic";
+import { redirect } from "next/navigation";
 
 const LearnPageContent = dynamic(() => import("@/components/learn/learn-page-content"), { loading: () => <Loading /> });
 
-export default function Page() {
-   
+export default async function Page() {
+    const user = await auth()
+    if (!user) return redirect("/")
+        
     return (
         <div className="w-full h-screen relative">
             <LearnPageContent />
