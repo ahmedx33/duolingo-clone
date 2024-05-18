df47f91209d7263e6cc0fb1fc307c5c01fe77efb
ahmedx33 <faresahmed51857@gmail.com>
2024-05-18 20:24:05 +0300

    feat: changed meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,18 @@ import { Metadata } from "next";
 
 import { Nunito } from "next/font/google";
 
-import { ClerkProvider, currentUser } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 import { Toaster } from "sonner";
 import StoreProvider from "@/components/providers/store-provider";
-import UserProgressProvider from "@/components/providers/user-progress-provider";
-import { prisma } from "@/db/db";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Duolingo Clone",
+    title: "Duolingo Clone - The world's best way to learn a language",
     description: "Learn and explore any lang!",
 };
 
